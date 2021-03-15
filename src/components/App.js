cf216f539e68ec5955a7831b5ecfbcfc77b0262b
Nghia <hello.nghia22@gmail.com>
2021-03-15 19:47:14 -0400

    feat: Add style and functionality to JobList

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import globeIcon from "assets/icons/globe.svg";
+import * as R from "ramda";
 
 export default () => {
 	return (
 		<div className="App">
 			<h1>Github Jobs</h1>
 			<Banner />
+			<JobsList />
 		</div>
 	);
 };
@@ -66,26 +68,57 @@ const Location = () => {
 };
 
 const JobsList = () => {
-	return (
-		<ul>
-			<JobCard />
-			<JobCard />
-			<JobCard />
-			<JobCard />
-			<JobCard />
-		</ul>
+	const [jobs, setJobs] = useState([]); // chunked into groups of 5
+	const [currentPage, setCurrentPage] = useState(0);
+	const [isLoadig, setIsLoading] = useState(false); // anthing that fetchs data will need loading state
+
+	const allOrigins = `https://api.allorigins.win/raw?url=`;
+	const config = {
+		jobsPerPage: 5,
+	};
+
+	useEffect(() => {
+		fetch(
+			`${allOrigins}https://jobs.github.com/positions.json?page=1&search=code`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				R.pipe(R.splitEvery(config.jobsPerPage), setJobs)(data);
+			});
+	}, []);
+
+	const renderedList = jobs[currentPage]?.map(
+		({ company_logo, company, title, type, location, created_at }) => {
+			return (
+				<li className="JobCard">
+					<img className="JobCard__company-logo" src={company_logo} />
+					<div>
+						<p>{company}</p>
+						<p>{title}</p>
+						<p>{type}</p>
+					</div>
+					<div className="JobCard__secondary-info">
+						<p>{location}</p>
+						<p>{created_at}</p>
+					</div>
+				</li>
+			);
+		}
 	);
+
+	console.log(jobs);
+	return <ul>{renderedList}</ul>;
 };
 
 const Search = () => {
 	const [searchTerm, setSearchTerm] = useState("");
-    console.log(searchTerm)
+
 	return (
 		<div className="Search">
 			<div className="Search__input-container">
 				<img className="Search__globe-icon" src={globeIcon} />
 				<input
-                    onChange={(e) => setSearchTerm(e.target.value)}
+					onChange={(e) => setSearchTerm(e.target.value)}
 					value={searchTerm}
 					className="Search__input"
 					placeholder="Title, companies, expertise or benefits"
