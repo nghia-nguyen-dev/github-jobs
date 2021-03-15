0cde6941491bab48bd21a1f777f2bcb3e76cf776
Nghia <hello.nghia22@gmail.com>
2021-03-15 18:30:53 -0400

    refactor: Change globe icon from inline svg to img element

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import globeIcon from "assets/icons/globe.svg"
+
 export default () => {
 	return (
 		<div className="App">
@@ -78,17 +81,11 @@ const Search = () => {
 	return (
 		<div className="Search">
 			<div className="Search__input-container">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					height="24"
-					viewBox="0 0 24 24"
-					width="24"
-                    className="Search__globe-icon"
-				>
-					<path d="M0 0h24v24H0z" fill="none" />
-					<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
-				</svg>
-				<input className="Search__input" placeholder="Title, companies, expertise or benefits"></input>
+                <img className="Search__globe-icon" src={globeIcon}/>
+				<input
+					className="Search__input"
+					placeholder="Title, companies, expertise or benefits"
+				></input>
 			</div>
 			<button className="Search__btn">Search</button>
 		</div>
