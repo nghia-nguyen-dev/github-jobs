0e51c82a2493e27fa94ecca25a3aef60d79b80cd
Nghia <hello.nghia22@gmail.com>
2021-03-15 17:09:03 -0400

    feat: Build static UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,93 @@
 export default () => {
-    return <div>App</div>
-}
+	return <div className="App">
+        <h1>Github Jobs</h1>
+        <Banner />
+    </div>;
+};
+
+const JobCard = () => {
+	return (
+		<div>
+			<img />
+			<div>
+				<p>Kasisto</p>
+				<p>Front-End Software Engineer</p>
+				<p>Full time</p>
+			</div>
+			<div>
+				<div>
+					<img />
+					<p>New York</p>
+				</div>
+				<div>
+					<img />
+					<p>5 days ago</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const PostDate = () => {
+    // some math operation to get num of days from the time it was posted
+
+	return (
+		<div>
+			<img />
+			<p>5 days ago</p>
+		</div>
+	);
+};
+
+const Fulltime = () => {
+	return (
+		<div>
+			<input type="checkbox" id="Fulltime" name="Fulltime" />
+			<label for="Fulltime">Fulltime</label>
+		</div>
+	);
+};
+
+const Location = () => {
+	return (
+		<section>
+			<h3>Location</h3>
+			<div>
+				<i></i>
+				<input></input>
+			</div>
+		</section>
+	);
+};
+
+const JobsList = () => {
+	return (
+		<ul>
+			<JobCard />
+			<JobCard />
+			<JobCard />
+			<JobCard />
+			<JobCard />
+		</ul>
+	);
+};
+
+const Search = () => {
+	return (
+		<div>
+			<div className="input-container">
+				<img />
+				<input placeholder="Title, companies, expertise or benefits"></input>
+			</div>
+			<button>Search</button>
+		</div>
+	);
+};
+
+const Banner = () => {
+	return <div className="Banner"></div>;
+};
+
+const Header = () => {
+	return <h1>Github Jobs</h1>;
+};
