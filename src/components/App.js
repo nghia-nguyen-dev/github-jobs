1cf8f9e72d5f27cb588d87ffac304f10b44a3180
Nghia <hello.nghia22@gmail.com>
2021-03-23 18:42:34 -0400

    refactor: Mv useEffect ƒ into Controller component

--- src/components/App.js
+++ src/components/App.js
@@ -157,6 +157,10 @@ const Controller = ({ setCurrentJob }) => {
 	const [pages, setPages] = useState([]);
 	const [currentPage, setCurrentPage] = useState(0);
 
+	useEffect(() => {
+		fetchData(setJobs);
+	}, []);
+
 	return (
 		<div className="Controller">
 			<Banner>
@@ -423,10 +427,6 @@ const fetchData = (setJobs, searchDescription = "") => {
 
 const Search = ({ setJobs }) => {
 	const [searchDescription, setSearchDescription] = useState("");
-
-	useEffect(() => {
-		fetchData(setJobs, searchDescription);
-	}, []);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
