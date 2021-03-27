84c1e694208dead0ba11fcbf4459737520a3d250
Nghia <hello.nghia22@gmail.com>
2021-03-27 16:11:49 -0400

    refactor: Hook up LocationInput to redux store

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,21 +1,30 @@
 import { useState, useEffect } from "react";
 import globeIcon from "assets/icons/globe.svg";
+import {setLocation} from 'store/filters'
+import {useDispatch, useSelector} from 'react-redux'
 
-const LocationInput = ({ setLocation, setCurrentPage }) => {
-	const [input, setInput] = useState(null);
 
-	useEffect(() => {
-		if (input !== null) {
-			const timeoutId = setTimeout(() => {
-				setLocation(input);
-				setCurrentPage(0);
-			}, 500);
+const LocationInput = () => {
+	const dispatch = useDispatch()
+	const input = useSelector(state => state.location)
+	// const [input, setInput] = useState(null);
 
-			return () => {
-				clearTimeout(timeoutId);
-			};
-		}
-	}, [input]);
+	// useEffect(() => {
+	// 	if (input !== null) {
+	// 		const timeoutId = setTimeout(() => {
+	// 			setLocation(input);
+	// 			setCurrentPage(0);
+	// 		}, 500);
+
+	// 		return () => {
+	// 			clearTimeout(timeoutId);
+	// 		};
+	// 	}
+	// }, [input]);
+
+	const handleInput = (e) => {
+		dispatch(setLocation(e.target.value))
+	}
 
 	return (
 		<div className="LocationInput">
@@ -26,7 +35,7 @@ const LocationInput = ({ setLocation, setCurrentPage }) => {
 					className="LocationInput__input"
 					placeholder="City, state, or country"
 					value={input}
-					onChange={(e) => setInput(e.target.value)}
+					onChange={handleInput}
 				/>
 			</div>
 		</div>
