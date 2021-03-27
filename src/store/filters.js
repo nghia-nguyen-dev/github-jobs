b41cb26a7f9579d64c8b82ea8d1328fcd2761998
Nghia <hello.nghia22@gmail.com>
2021-03-27 15:50:53 -0400

    feat: Create filters reducer

---
+++ src/store/filters.js
@@ -0,0 +1,28 @@
+// INIT
+const init = {
+	isFullTime: false,
+	location: "",
+};
+
+// ACTION CREATOR
+export const toggleFullTime = () => ({ type: "TOGGLE_FULLTIME" });
+export const setLocation = (location) => ({ type: "SET_LOCATION", location });
+
+// REDUCER
+export default filtersReducer = (state = init, action) => {
+	switch (action.type) {
+		case "TOGGLE_FULLTIME":
+			return {
+				...state,
+				isFullTime: !state.isFullTime,
+			};
+		case "SET_LOCATION":
+			return {
+				...state,
+				location: action.location,
+			};
+
+		default:
+			return state;
+	}
+};
