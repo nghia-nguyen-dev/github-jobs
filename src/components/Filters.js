00948d3ff451dcccf898d5abd599156703949b65
Nghia <hello.nghia22@gmail.com>
2021-03-28 12:06:21 -0400

    feat: Increase banner height

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,33 +1,7 @@
-import { curryfullTimeFilter, curryLocationInput } from "utils/helpers";
-import { useState, useEffect } from "react";
-import * as R from "ramda";
-import config from "utils/config";
 import FullTime from "components/FullTime";
 import LocationInput from "components/LocationInput";
 
 const Filters = () => {
-
-	// useEffect(() => {
-	// 	const data = localStorage.getItem("full-time");
-	// 	if (data) {
-	// 		setFullTime(JSON.parse(data));
-	// 	}
-	// }, []);
-
-	// useEffect(() => {
-	// 	localStorage.setItem("full-time", fullTime);
-	// }, [fullTime]);
-
-	// useEffect(() => {
-	// 	!R.isEmpty(jobs) &&
-	// 		R.pipe(
-	// 			curryfullTimeFilter(fullTime, R.__),
-	// 			curryLocationInput(location, R.__),
-	// 			R.splitEvery(config.jobsPerPage),
-	// 			setPages
-	// 		)(jobs);
-	// }, [jobs, fullTime, location]);
-
 	return (
 		<div className="Filters">
 			<FullTime />
